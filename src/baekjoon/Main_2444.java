a6ac4a298149d537ddef1e3f9e5a3de1b5241d17
이동욱 <dkdl8412@gmail.com>
2024-07-29 15:51:48 +0900

    Solved BaekJoon p.2444

--- src/baekjoon/Main_2444.java
+++ src/baekjoon/Main_2444.java
@@ -7,9 +7,14 @@ public class Main_2444 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		//모루게따asd
-		for(int i=0; i<2*N-1; i++) {
-			
+		for(int i=1; i<=2*N-1; i++) {
+			for(int j=1; j<=Math.abs(N-i); j++) {    // 공백이 4,3,2,1,0,1,2,3,4 니까 math.abs(N-i)
+				System.out.print(" ");
+			}
+			for(int j=Math.abs(N-i)+1; j<2*N-Math.abs(N-i); j++) { // 별찍기 해당 위치부터 1 3 5 7 9 7 5 3 1 니까 2N-1-|N-i|
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
