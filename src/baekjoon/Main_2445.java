d96be79b39ea61da8c646fc95996fa57ac3e2416
이동욱 <dkdl8412@gmail.com>
2024-07-30 09:16:34 +0900

    [BJ] Solved p.2445

--- src/baekjoon/Main_2445.java
+++ src/baekjoon/Main_2445.java
@@ -3,10 +3,24 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main_2445 { // 별찍기 8번 https://www.acmicpc.net/problem/2445
+public class Main_2445 { 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		
+		int N = Integer.parseInt(br.readLine());
+		for(int i=1; i<2*N; i++) {
+			for(int j=1; j<=N-Math.abs(N-i); j++) {
+				sb.append("*");
+			}
+			for(int j=1; j<=2*Math.abs(N-i); j++) {
+				sb.append(" ");
+			}
+			for(int j=1; j<=N-Math.abs(N-i); j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
 	}
 }
