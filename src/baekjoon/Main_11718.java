3a847d75a0d28332b933150686687b31f5fcd12f
2Ludy <dkdl8412@gmail.com>
2024-07-31 19:13:07 +0900

    [BJ] Solved p.10951(NullPointer)

--- src/baekjoon/Main_11718.java
+++ src/baekjoon/Main_11718.java
@@ -2,13 +2,17 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main_10951 { // BufferedReader에서 값이 없어 null 값이 input 될때 예외처리
+public class Main_11718 { // BufferedReader에서 값이 없어 null 값이 input 될때 예외처리
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line ="";
 		while((line = br.readLine()) != null) { // BufferedReader에서 값이 없어 null 값이 input 될때 예외처리
-			System.out.println(line);
+			StringTokenizer st = new StringTokenizer(line);
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			System.out.println(num1 + num2);
 		}
 	}
 }
