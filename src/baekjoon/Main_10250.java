41ed14b706416f370e9fd5de5185fe3b33efab3f
이동욱 <dkdl8412@gmail.com>
2024-08-01 16:08:49 +0900

    [BJ] Solved p.10250

--- src/baekjoon/Main_10250.java
+++ src/baekjoon/Main_10250.java
@@ -2,26 +2,33 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main_10250 { 
 	
-	static char[] cs;
-	static int[] alpha;
+	static int T, H, W, N, floor, ho;
+	static String sho;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		alpha = new int[26]; // 알파벳 총 26개
-		Arrays.fill(alpha, -1);
-		cs = br.readLine().toCharArray();
-		for(int i=0; i<cs.length; i++) {
-			if(alpha[cs[i]-'a'] == -1) {
-				alpha[cs[i]-'a'] = i;
-			}
-		}
+		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<alpha.length; i++) {
-			sb.append(alpha[i]).append(" ");
+		
+		for(int t=0; t<T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			floor = N%H;
+			if(floor == 0) floor = H;
+			ho = N/H +1;
+			if(floor == H) ho--;
+			sho = Integer.toString(ho);
+			
+			if(ho < 10) {
+				sho = "0" + sho;
+			}
+			sb.append(floor).append(sho).append("\n");
 		}
 		sb.delete(sb.length()-1, sb.length());
 		System.out.println(sb);
