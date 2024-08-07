de820f9d6fc536216c525767b4f381bf2969f62c
이동욱 <dkdl8412@gmail.com>
2024-08-07 11:29:23 +0900

    [BJ] Solved p.10814

--- src/baekjoon/Main_10814.java
+++ src/baekjoon/Main_10814.java
@@ -8,28 +8,30 @@ import java.util.StringTokenizer;
 
 public class Main_10814 { 
 	
-	static int N;
+	static int N, k;
 	static String[][] strs;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		strs = new String[N][2];
+		strs = new String[N][3];
+		k=1;
 		
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			strs[i][0] = st.nextToken();
 			strs[i][1] = st.nextToken();
+			strs[i][2] = Integer.toString(k++);
 		}
 		
 		Arrays.sort(strs, new Comparator<String[]>() {
 
 			@Override
 			public int compare(String[] o1, String[] o2) {
-				if(o1[0].compareTo(o2[0]) == 0) {
-					return o1.length - o2.length;
+				if(Integer.parseInt(o1[0]) == Integer.parseInt(o2[0])) {
+					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
 				}
-				return o1[0].compareTo(o2[0]);
+				return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
 			}
 		});
 		
