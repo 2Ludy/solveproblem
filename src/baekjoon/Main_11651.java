ddac817fe4142e0cff56692db2f4622123981700
이동욱 <dkdl8412@gmail.com>
2024-08-09 14:39:34 +0900

    [BJ] Solved p.11651

---
+++ src/baekjoon/Main_11651.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+
+public class Main_11651 {
+	
+	static int N;
+	static int[][] nums;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N][2];
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			nums[i][0] = Integer.parseInt(st.nextToken());
+			nums[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
+			}
+			});
+		
+		for(int i=0; i<N; i++) {
+			sb.append(nums[i][0]).append(" ").append(nums[i][1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
