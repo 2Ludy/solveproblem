1e24729fbd889c996832ecb1f0c295159142156f
이동욱 <dkdl8412@gmail.com>
2024-08-16 11:11:19 +0900

    [BJ] Solved p.2669

--- src/baekjoon/Main_2669.java
+++ src/baekjoon/Main_2669.java
@@ -6,13 +6,33 @@ import java.util.StringTokenizer;
 
 public class Main_2669 {
 	
-	static int T;
-	static int R;
-	static char[] cs;
+	static int[][] nums, square;
+	static int N = 4;
+	static int count;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+		 nums = new int[N][N];
+		 for(int i=0; i<N; i++) {
+			 StringTokenizer st = new StringTokenizer(br.readLine());
+			 for(int j=0; j<N; j++) {
+				 nums[i][j] = Integer.parseInt(st.nextToken());
+			 }
+		 } // 좌표 받기
+		 square = new int[101][101];
+		 for(int i=0; i<N; i++) {
+			 for(int r = nums[i][0]; r<nums[i][2]; r++) {
+				 for(int c = nums[i][1]; c< nums[i][3]; c++) {
+					 square[r][c] = 1;
+				 }
+			 }
+		 }
+		 count = 0;
+		 for(int i=0; i<101; i++) {
+			 for(int j=0; j<101; j++) {
+				 if(square[i][j] == 1) count++;
+			 }
+		 }
+		 System.out.println(count);
 	}
 }
