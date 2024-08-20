9441e333fdc7082bfc72775c6afa98aff00075fd
이동욱 <dkdl8412@gmail.com>
2024-08-20 14:11:18 +0900

    [BJ] Solved p.2309

--- src/baekjoon/Main_2309.java
+++ src/baekjoon/Main_2309.java
@@ -2,18 +2,47 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main_2309 {
 	
 	static int[] nums;
+	static int[] seven;
+	static int count;
+	static int hap;
 	
 	public static void main(String[] args) throws Exception{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		nums = new int[9];
+		
 		for(int i=0; i<9; i++) {
 			nums[i] = Integer.parseInt(br.readLine());
 		}
 		
+		seven = new int[7];
 		
+		nCr(0,0);
+		Arrays.sort(seven);
+		
+		for(int num : seven) {
+			System.out.println(num);
+		}
+	}
+
+	private static void nCr(int cnt, int start) {
+		if(cnt == 7) {
+			hap = 0;
+			for(int a : seven) {
+				hap += a;
+			}
+			return;
+		}
+		
+		for(int i=start; i<9; i++) {
+			if(hap == 100) return;
+			seven[cnt] = nums[i];
+			nCr(cnt+1,i+1);
+		}
 	}
 }
