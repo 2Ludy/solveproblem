a1af971c82ebbc66e1add6b5374bfe21c7d64829
이동욱 <dkdl8412@gmail.com>
2024-08-22 08:59:43 +0900

    [BJ] Solved p.15649

---
+++ src/baekjoon/Main_15649.java
@@ -0,0 +1,51 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15649 {
+	
+	static int N, M;
+	static StringBuilder sb;
+	static int[] nums;
+	static int[] p;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		for(int i=0; i<N; i++) {
+			nums[i] = i+1;
+		}
+		
+		p = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+		nPr(0);
+		System.out.println(sb);
+	}
+
+	static void nPr(int cnt) {
+		if(cnt == M) {
+			for(int num : p) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(visited[i]) continue;
+			visited[i] = true;
+			p[cnt] = nums[i];
+			nPr(cnt+1);
+			p[cnt] = 0;
+			visited[i] = false;
+		}
+	}
+
+}
