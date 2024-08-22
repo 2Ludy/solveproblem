7f5dc2c120a1121c562554582c8431b96d1a4b6f
이동욱 <dkdl8412@gmail.com>
2024-08-22 13:06:31 +0900

    [BJ] Solved p.1927

---
+++ src/baekjoon/Main_1927.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main_1927 {
+	
+	static int N, num;
+	static PriorityQueue<Integer> pq;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		pq = new PriorityQueue<>();
+		N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			num = Integer.parseInt(br.readLine());
+			if(num == 0) {
+				if(pq.isEmpty()) {
+					sb.append(0).append("\n");
+				}else {
+					sb.append(pq.poll()).append("\n");
+				}
+			}else {
+				pq.offer(num);
+			}
+		}
+		System.out.println(sb);
+	}
+}
