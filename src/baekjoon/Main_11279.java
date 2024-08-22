efb2c447d63d09a9cde2da4e38aac245c6710cff
이동욱 <dkdl8412@gmail.com>
2024-08-22 13:09:13 +0900

    [BJ] Solved p.11279

--- src/baekjoon/Main_11279.java
+++ src/baekjoon/Main_11279.java
@@ -2,6 +2,7 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main_11279 {
@@ -11,7 +12,13 @@ public class Main_11279 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		pq = new PriorityQueue<>();
+		pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2-o1;
+			}});
+		
 		N = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<N; i++) {
