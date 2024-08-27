940c184b92302d22c737248e0906028edfbac116
이동욱 <dkdl8412@gmail.com>
2024-08-27 14:40:44 +0900

    [BJ] Solved p.15652

---
+++ src/baekjoon/Main_15652.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15652 {
+	
+	static int N, M;
+	static StringBuilder sb;
+	static int[] p;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	
+		p = new int[M];
+		sb = new StringBuilder();
+		nHr(0, 0);
+		System.out.println(sb);
+	}
+
+	static void nHr(int idx, int cnt) {
+		if(idx == M) {
+			for(int num : p) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=cnt; i<N; i++) {
+			p[idx] = i+1;
+			nHr(idx+1, i);
+			p[idx] = 0;
+		}
+	}
+}
