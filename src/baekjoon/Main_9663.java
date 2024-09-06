4d901075ddcfbb96f8f06a51958eaebba5590b08
dkdl8412 <dkdl8412@gmail.com>
2024-09-06 17:28:27 +0900

    [BJ] Solved p.9663

--- src/baekjoon/Main_9663.java
+++ src/baekjoon/Main_9663.java
@@ -5,24 +5,39 @@ import java.io.InputStreamReader;
 
 public class Main_9663 {
 	
-	static int N, count;
-	static int[][] chess;
-	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+	static int N;
+	static int[] chess;
+	static int count;
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		chess = new int[N][N];
 		
+		chess = new int[N];
+			
 		count = 0;
-		nqueen(0,0);
+		queen(0);
 		System.out.println(count);
 	}
 
-	static void nqueen(int cnt, int c) {
-		if(cnt == N) {
-			
+	static void queen(int idx) {
+		if(idx == N) {
+			count++;
+			return;
 		}
+		
+		for(int i=0; i<N; i++) {
+			if(check(i, idx)) {
+				chess[idx] = i;
+				queen(idx+1);
+			}
+		}
+	}
+
+	static boolean check(int r, int idx) {
+		for(int i=0; i<idx; i++) {
+			if(chess[i] == r || Math.abs(chess[i]-r) == Math.abs(idx-i)) return false;
+		}
+		return true;
 	}
 }
