ff378a8b10b4a2bfb90eb5f49e5927a93250997d
2Ludy <dkdl8412@gmail.com>
2024-10-25 16:56:52 +0900

    [BJ] Memory Boom p.2638

--- src/baekjoon/Main_2638.java
+++ src/baekjoon/Main_2638.java
@@ -2,9 +2,7 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -14,7 +12,7 @@ public class Main_2638 {
 	static int[][] copyPan;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
-	static int N, M, count;
+	static int N, M, count, time;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,14 +26,73 @@ public class Main_2638 {
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
+				pan[i][j] = Integer.parseInt(st.nextToken());
 				if(pan[i][j] == 1) {
-					pan[i][j] = Integer.parseInt(st.nextToken());
 					count++;
 				}
 			}
-			copyPan[i] = pan[i].clone();
 		}
 		
+		Queue<int[]> que = new LinkedList<>();
+		que.offer(new int[] {0,0});
+		pan[0][0] = 2;
+		while(!que.isEmpty()) {
+			int[] nums = que.poll();
+			int r = nums[0];
+			int c = nums[1];
+			for(int d=0; d<4; d++) {
+				int nr = r+dr[d];
+				int nc = c+dc[d];
+				if(!check(nr,nc)) continue;
+				if(pan[nr][nc] != 0) continue;
+				pan[nr][nc] = 2;
+				que.offer(new int[] {nr,nc});
+			}
+		}
 		
+		time = 0;
+		while(count > 0) {
+			clonePan();
+			logic();
+			time++;
+			if(count == 0) break;
+			reClonePan();
+		}
+		
+		System.out.println(time);
+	}
+
+	private static void logic() {
+		for(int r=1; r<N-1; r++) {
+			for(int c=1; c<M-1; c++) {
+				if(pan[r][c] == 1) {
+					int cnt = 0;
+					for(int d=0; d<4; d++) {
+						int nr = r+dr[d];
+						int nc = c+dc[d];
+						if(!check(nr,nc)) continue;
+						if(pan[nr][nc] != 2) continue;
+						cnt++;
+					}
+					if(cnt > 1) {
+						copyPan[r][c] = 2;
+						count--;
+					}
+				}
+			}
+		}
+	}
+
+	private static void reClonePan() {
+		for(int i=0; i<N; i++) pan[i] = copyPan[i].clone();
+	}
+
+	private static void clonePan() {
+		for(int i=0; i<N; i++) copyPan[i] = pan[i].clone();
+		
+	}
+
+	private static boolean check(int r, int c) {
+		return r>=0 && r<N && c>=0 && c<M;
 	}
 }
