2cf1137ac1c4cf29a832b1f79cbb4f2867fa90b9
2Ludy <dkdl8412@gmail.com>
2025-02-25 17:27:48 +0900

    [BJ] Solved p.1463

--- src/baekjoon/Main_1463.java
+++ src/baekjoon/Main_1463.java
@@ -2,30 +2,31 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main_1463 {
-	
-	static int N, count;
-	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-//		// 1순위 3나누기 2순위 2로 나누기 3순위 1빼기
-//		count = 0;
-//		while(N>1) {
-//			if(N%3 == 0) {
-//				N = N/3;
-//			}else if(N%2 == 0) {
-//				N = N/2;
-//			} else {
-//				N--;
-//			}
-//			System.out.println(N);
-//			count++;
-//		}
-////		System.out.println(count);
+		int N = Integer.parseInt(br.readLine());
+
+		// dp[i] = i를 1로 만드는데 필요한 최소 연산 횟수
+		int[] dp = new int[N + 1];
+
+		// 상향식(Bottom-up) 접근
+		for (int i = 2; i <= N; i++) {
+			// 1을 빼는 연산
+			dp[i] = dp[i - 1] + 1;
+
+			// 2로 나누어 떨어지는 경우
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+
+			// 3으로 나누어 떨어지는 경우
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+		}
+
+		System.out.println(dp[N]);
 	}
 }
